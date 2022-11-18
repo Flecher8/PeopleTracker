a2e9002a9c6ca9e8b1a7fc0644bc499c19294466
Vlad Bocharov <vladislavboc11@gmail.com>
2022-11-18 14:25:35 +0200

    Addd main page with localization

--- frontend/src/pages/Contacts/Contacts.js
+++ frontend/src/pages/Contacts/Contacts.js
@@ -6,22 +6,16 @@ import axios from "../../api/axios";
 import text from "../../services/localizationService";
 
 function Contacts() {
-	useEffect(() => {}, []);
-
-	const submit = async e => {
-		// if (checkValidDateTime()) {
-		// 	await getData();
-		// }
-	};
-
 	return (
 		<div className="container">
 			<div className="d-flex justify-content-center display-4">{text("Contacts")}</div>
 			<div className="container mt-3 border border-dark">
 				{/* // TODO language */}
-				<h3>Done by Vladyslav Bocharov</h3>
+				<h3>
+					{text("Done by")} {text("Vladyslav Bocharov")}
+				</h3>
 				{/* // TODO language */}
-				<h5>Email: vladyslav.bocharov@nure.ua</h5>
+				<h5>{text("Email")}: vladyslav.bocharov@nure.ua</h5>
 			</div>
 		</div>
 	);
--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -6,22 +6,32 @@ import axios from "../../api/axios";
 import text from "../../services/localizationService";
 
 function Main() {
-	useEffect(() => {}, []);
-
-	const submit = async e => {
-		// if (checkValidDateTime()) {
-		// 	await getData();
-		// }
-	};
-
 	return (
 		<div className="container">
 			<div className="d-flex justify-content-center display-4">{text("Main page")}</div>
 			<div className="container mt-3 border border-dark">
-				{/* // TODO language */}
-				<h3>Done by Vladyslav Bocharov</h3>
-				{/* // TODO language */}
-				<h5>Email: vladyslav.bocharov@nure.ua</h5>
+				<div className="container mt-3 ">
+					{/* // TODO language */}
+					<h3>{text("Product description")}</h3>
+					{/* // TODO language */}
+					{/* MainText */}
+					<p>{text("MainText")}</p>
+				</div>
+				<div className="container mt-5">
+					{/* // TODO language */}
+					<h5>{text("Smart device price")}: 10$</h5>
+				</div>
+				<div className="container mt-3">
+					{/* // TODO language */}
+					<h5>
+						{text("Subscription price")}: 10$ / {text("month")}
+					</h5>
+				</div>
+				<div className="container mt-5">
+					{/* // TODO language */}
+					{/* ContactsText */}
+					<p>{text("ContactsText")}</p>
+				</div>
 			</div>
 		</div>
 	);
